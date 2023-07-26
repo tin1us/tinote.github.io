6353f5619cc219d9793546f8d98eeb593a7729de
tin1us <113222129+tin1us@users.noreply.github.com>
2023-07-26 13:41:55 +0800

    Add files via upload

---
+++ Search/script.js
@@ -0,0 +1,45 @@
+﻿charset="utf-8";
+const searchData = [
+  { title: '阅读须知', url: 'https://tin1us.github.io/tinote.github.io/readme' },
+  { title: '哦！大青虫！垃圾桶！复兴号CR200j动力集中式动车组列车深度测评。', url: 'https://tin1us.github.io/tinote.github.io/200jcd' },
+  { title: '【CRVlog】5631次 SS7C本务 25G车厢 达州～重庆北运转记录', url: 'https://tin1us.github.io/tinote.github.io/25gcd' },
+  { title: '我用最便宜的方式摆烂了你的ae作业', url: 'https://tin1us.github.io/tinote.github.io/2023nbl' }
+];
+
+const searchInput = document.getElementById('search-input');
+const searchResults = document.getElementById('search-results');
+
+searchInput.addEventListener('input', function() {
+  const keyword = this.value.trim().toLowerCase();
+  if(keyword.length > 0) {
+    const filteredData = searchData.filter(function(item) {
+      return item.title.toLowerCase().includes(keyword);
+    });
+    
+    displayResults(filteredData);
+  } else {
+    clearResults();
+  }
+});
+
+function displayResults(results) {
+  searchResults.innerHTML = '';
+  
+  results.forEach(function(item) {
+    const li = document.createElement('li');
+    li.textContent = item.title;
+    
+    li.addEventListener('click', function() {
+      window.location.href = item.url;
+    });
+    
+    searchResults.appendChild(li);
+  });
+  
+  searchResults.style.display = 'block';
+}
+
+function clearResults() {
+  searchResults.innerHTML = '';
+  searchResults.style.display = 'none';
+}
